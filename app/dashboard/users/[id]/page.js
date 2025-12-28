010f0b5afcd896a31c63cd30979d7fcb81de7a4a
nitin2004kr <nitin112004kr@gmail.com>
2025-12-28 11:31:49 +0530

    updated code

--- app/dashboard/users/[id]/page.js
+++ app/dashboard/users/[id]/page.js
@@ -4,7 +4,12 @@ import styles from '../../../ui/dashboard/users/singleUser/singleUser.module.css
 const SingleUser = () => {
   return (
     <div className={styles.container}>
-       
+       <div className={styles.imageCon}>
+            
+       </div>
+       <div className={styles.userInfoCon}>
+           
+       </div>
     </div>
   )
 }
